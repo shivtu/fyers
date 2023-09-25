601f7a866f5b1f3177145547ec6ddea966decc49
Shiben Dutta <shibendutta@gmail.com>
2023-09-25 14:24:30 +0530

    Update constants.ts

--- utils/constants.ts
+++ utils/constants.ts
@@ -1,6 +1,6 @@
-const APP_ID = '1PDMEZRGFR-100';
+const APP_ID = '';
 const FYERS_BASE_API_URI = 'https://api.fyers.in/api/v2/';
-const CLIENT_SECRET = 'BYX7B2BVCO';
+const CLIENT_SECRET = '';
 const REDIRECT_URI = 'http://localhost:3000/api/fyers/auth';
 const ROUTES = {
   HOME: '/home',
